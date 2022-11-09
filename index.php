1ff2084b8cd36e7603217466a6159aa12940a02a
student-pvpit <94478869+student-pvpit@users.noreply.github.com>
2022-11-09 10:38:39 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>My APP1</tittle>
+</head>
+<body>
+<h1 style="color:Red;">Welcome to My Web Appliocation</h1>
+<?php
+echo "<h1>This App is created using Azure App Service</h1>";
+?>
+</body>
+</html>
